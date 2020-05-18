c02ccb8394d3af6379211ade40518c514eae7de8
Prince Omonu <omonuitanyi@gmail.com>
2020-05-18 03:47:30 +0100

    commit one

---
+++ precache-manifest.87c5cb28d9931f7107360a64db2f687d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ba069d37ded0bd7673ab6ce5c2daba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc9a72385a8b9879bcce",
+    "url": "./static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "34df56de6a3f0405f073",
+    "url": "./static/css/main.160e4576.chunk.css"
+  },
+  {
+    "revision": "dc9a72385a8b9879bcce",
+    "url": "./static/js/2.eac8c718.chunk.js"
+  },
+  {
+    "revision": "34df56de6a3f0405f073",
+    "url": "./static/js/main.55b4f5f2.chunk.js"
+  },
+  {
+    "revision": "68475b7496def902d754",
+    "url": "./static/js/runtime~main.25d00d7e.js"
+  }
+]);
